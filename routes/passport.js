a31879962c828533176db39465f511d18b0311af
arpit.nagpal <arpit.nagpal@jungleworks.com>
2018-06-28 17:06:21 +0530

    Added passpoet middleware and constants file

---
+++ routes/passport.js
@@ -0,0 +1,47 @@
+const passport = require("passport");
+const localStratergy = require("passport-local").Strategy();
+const user = require("../models/user");
+const constants = require("../constants");
+
+//serialize and deserialize
+passport.serializeUser(function(user, callback){
+    callback(null, user._id)
+})
+
+passport.deserializeUser(function(id, cb){
+    user.findById(id, function(err, user){
+        cb(null, user)
+    })
+})
+
+
+//middleware
+passport.use("local-login", new localStorage({
+    usernameField: constants.passportMiddleware.USERNAME_FIELD,
+    passwordField: constants.passportMiddleware.PASSWORD_FIELD,
+    passReqToCallback: constants.passportMiddleware.PASS_REQ_TO_CALLBACK
+}, function(req, email, password, callback){
+    user.findOne({email: email}, function(error, user){
+        if(error) return callback(error)
+        
+        if(!user){
+            return callback(null, false, req.flash("lognMessage", constants.flash.NO_USER))
+        }
+
+        if(!user.comparePassword(password)){
+            return callback(null, false, req.flash("loginMessage", constants.flash.WRONG_PASSWORD))
+        }
+
+        return callback(null, user)
+
+    })
+}))
+
+
+//custom function to authenticate
+exports.isAuthenticated(function(req, res, callback){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/login")
+})
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const userModel = require("../models/user")
 const boom = require("boom");
+const constants = require("../constants");
 
 router.get("/signup", function(req, res){
     res.render("accounts/signup", {
@@ -17,12 +18,12 @@ router.post("/signup", function(req, res, next){
 
     userModel.findOne({email: req.body.email} , function(error, exists){
         if(exists) {
-            req.flash("errors", `Account with email: ${req.body.email} already exists`)
+            req.flash("errors", constants.flash.EMAIL_ALREADY_EXISTS)
             return res.redirect("/signup")
         }
         else{
             user.save(function(err, user){
-                if(err) return boom.badRequest("Unable to signup")
+                if(err) return boom.badRequest(constants.flash.SIGNUP_ERROR)
                 else {
                     return res.redirect("/")
                 }
